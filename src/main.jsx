4ceb28485a4fba591d3a7f4601cdadea0eb1c1a1
mugimagu <bgatunamagu0001@gmail.com>
2026-04-29 17:11:36 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // もしCSSファイルがあれば
+
+// エラーの原因となっていた './index.css' のインポート行を削除しました。
+// Tailwind CSSはindex.html側で読み込んでいるため、このままで動作します。
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

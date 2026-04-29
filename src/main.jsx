8ec094d2d1c38873e316b5d7a0f5aefb83cfa1f5
mugimagu <bgatunamagu0001@gmail.com>
2026-04-29 17:09:52 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // もしあれば
+import './index.css' // もしCSSファイルがあれば
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
